a7b48419f1430bfcb6de62cac60d455562c0cfe7
Abhijeet Rastogi <abhijeet.1989@gmail.com>
2026-03-12 21:38:21 -0700

    hammerspoon(mouse): fix bug when circle is sometimes not deleted

--- dot_hammerspoon/private_mouse.lua
+++ dot_hammerspoon/private_mouse.lua
@@ -2,16 +2,26 @@
 
 local Mouse = {}
 
+local _circle = nil
+local _timer = nil
+
 -- highlight mouse with a red circle
 function Mouse.highlight()
+    -- Clean up previous highlight
+    if _timer then _timer:stop() end
+    if _circle then _circle:delete() end
+
     local mousepoint = hs.mouse.absolutePosition()
-    local mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
-    if mouseCircle then
-        mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
-        mouseCircle:setFill(false)
-        mouseCircle:setStrokeWidth(5)
-        mouseCircle:show()
-        hs.timer.doAfter(1, function() mouseCircle:delete() end)
+    _circle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
+    if _circle then
+        _circle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
+        _circle:setFill(false)
+        _circle:setStrokeWidth(5)
+        _circle:show()
+        _timer = hs.timer.doAfter(1, function()
+            if _circle then _circle:delete(); _circle = nil end
+            _timer = nil
+        end)
     end
 end
 
