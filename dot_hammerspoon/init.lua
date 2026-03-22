7a7434d4c4f9ef08238eeccffb9a52c86e0b8fb2
Abhijeet Rastogi <abhijeet.1989@gmail.com>
2026-03-22 04:29:28 -0700

    hammerspoon: copy selection automatically

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -7,6 +7,8 @@ require "win/switch"
 require "win/spaces"
 require "win/auto_close"
 require "audio_watcher"
+require "selectcopy"
+require "hs.ipc"
 
 -- Auto-install and load spoons
 hs.loadSpoon("SpoonInstall")
