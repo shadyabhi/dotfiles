f70d21a972283008e59cd16c71566701db26b36a
Abhijeet Rastogi <abhijeet.1989@gmail.com>
2026-04-27 19:41:23 -0700

    nvim: add neo-tree file explorer and thicker pane separators
    Auto-opens neo-tree on VimEnter with <leader>e/<leader>o keymaps.
    Sets heavy box-drawing fillchars and a bold blue WinSeparator
    highlight for clearer pane boundaries.

---
+++ private_dot_config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,41 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  cmd = "Neotree",
+  keys = {
+    { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Toggle file explorer" },
+    { "<leader>o", "<cmd>Neotree focus<cr>", desc = "Focus file explorer" },
+  },
+  opts = {
+    close_if_last_window = true,
+    filesystem = {
+      follow_current_file = { enabled = true },
+      hijack_netrw_behavior = "open_default",
+    },
+    window = {
+      width = 32,
+    },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        if vim.fn.argc() == 0 then
+          vim.cmd("Neotree show")
+        else
+          local arg = vim.fn.argv(0)
+          if vim.fn.isdirectory(arg) == 1 then
+            vim.cmd("Neotree show dir=" .. vim.fn.fnameescape(arg))
+          else
+            vim.cmd("Neotree show")
+            vim.cmd("wincmd p")
+          end
+        end
+      end,
+    })
+  end,
+}
--- private_dot_config/nvim/lua/plugins/tokyonight.lua
+++ private_dot_config/nvim/lua/plugins/tokyonight.lua
@@ -11,6 +11,9 @@ return {
     config = function(_, opts)
       require("tokyonight").setup(opts)
       vim.cmd.colorscheme("tokyonight")
+
+      vim.opt.fillchars:append({ vert = "┃", vertleft = "┫", vertright = "┣", verthoriz = "╋", horiz = "━", horizup = "┻", horizdown = "┳" })
+      vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#7aa2f7", bold = true })
     end,
   },
 }
